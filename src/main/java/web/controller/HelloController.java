7d0117073ebba22cac067db9113acc7e9ef67337
Yuriy <39201286+yuri2711@users.noreply.github.com>
2021-04-13 15:47:03 +0300

    Add files via upload

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,9 +1,12 @@
 package web.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import web.model.Car;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,9 +19,24 @@ public class HelloController {
 		List<String> messages = new ArrayList<>();
 		messages.add("Hello!");
 		messages.add("I'm Spring MVC application");
-		messages.add("5.2.0 version by sep'19 ");
+		messages.add("Ну ты ...  ");
 		model.addAttribute("messages", messages);
 		return "index";
 	}
+
+	@GetMapping(value = "/cars")
+	public String printCar(@RequestParam(value = "count", required = false) int size, ModelMap modelMap) {
+		List<Car> list = new ArrayList<>();
+		list.add(new Car("BMW", 500, 2));
+		list.add(new Car("BMW", 500, 2));
+		list.add(new Car("BMW", 500, 2));
+		list.add(new Car("BMW", 500, 2));
+		if(size==0 || size>=5){
+			modelMap.addAttribute("table", new String[]{"model", "series", "speed"});
+		}
+		return "car";
+	}
+
+
 	
 }
